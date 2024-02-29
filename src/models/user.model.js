17ff803e2a46104ba7ee72cc7bcbb22ea86a02c0
MRam34272 <mram34272@gmail.com>
2024-02-29 21:30:58 +0530

    Completed User Model.

---
+++ src/models/user.model.js
@@ -0,0 +1,74 @@
+import mongoose,{ Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    userName : {
+      type:String,
+      required:true,
+      unique:true,
+      lowercase:true,
+      trim:true,
+      index:true
+    },
+    name:{
+      type:String,
+      required:true,
+      trim:true,
+      index:true
+    },
+    email:{
+      type:String,
+      required:true,
+      unique:true,
+      trim:true,
+    },
+    passWord:{
+      type:String,
+      required: [true , "Password is required!"]
+    },
+    refreshToken:{
+      type:String
+    }
+  },{ timestamps:true }
+)
+
+// Method to check whether password is modified or not, if not hash the password
+userSchema.pre("save", async () => {
+  if(!this.isModified("passWord")) return next();
+  this.passWord = await bcrypt.hash(this.passWord , 10);
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async (password) => {
+  return await bcrypt.compare(password,this.passWord)
+}
+
+userSchema.methods.generateAccessToken = () => {
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      userName:this.userName,
+      name:this.name
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = () => {
+  return jwt.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User",userSchema);
