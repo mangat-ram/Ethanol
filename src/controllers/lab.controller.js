ab982c19e1375cfabed248535bdb4dae3d74d664
mangat-ram <se.mangat.ram@gmail.com>
2024-03-07 23:10:11 +0530

    started writing update labname logic.

--- src/controllers/lab.controller.js
+++ src/controllers/lab.controller.js
@@ -1,28 +1,9 @@
 import { Lab } from "../models/lab.model.js";
-import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-
-// const createLab = asyncHandler(async(req, res) => {
-
-//   const { labName } = req.body;
-//   const lab = await Lab.create({labName:labName});
-//   const userId = req.user._id;
-//   const user = await User.updateUserForLabCreation(userId, lab._id);
-
-//   if(!labName){
-//     throw new ApiError(401,"Labname is Required.")
-//   }
-
-//   return res
-//   .status(201)
-//   .json(
-//     new ApiResponse(200,lab,"Lab created Successfully.")
-//   )
-// })
 
 const createLab = asyncHandler(async(req,res) => {
   const { labName } = req.body;
@@ -44,6 +25,25 @@ const createLab = asyncHandler(async(req,res) => {
   )
 })
 
+const updateLabname = asyncHandler(async(req, res) => {
+  const { newLabname } = req.body;
+  const userId = req.user._id;
+  const userName = req.user.userName
+  console.log(userId);
+  console.log(userName);
+  // const lab = await Lab.aggregate(
+  //   [
+  //     {
+  //       $match:{
+  //         labMaker:userId,
+
+  //       }
+  //     }
+  //   ]
+  // )
+})
+
 export{
-  createLab
+  createLab,
+  updateLabname
 }
--- src/routes/lab.routes.js
+++ src/routes/lab.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { createLab } from "../controllers/lab.controller.js";
+import { 
+    createLab,
+    updateLabname
+  } from "../controllers/lab.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -7,6 +10,7 @@ const router = Router();
 
 // secured Routes
 router.route("/createLab").post(verifyJWT, createLab);
+router.route("/updateLabname").patch(verifyJWT, updateLabname);
 
 export default router;
 
