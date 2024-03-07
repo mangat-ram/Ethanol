7aadc68d37459d6b719167cb9b7b34044b068bbf
mangat-ram <se.mangat.ram@gmail.com>
2024-03-07 20:54:12 +0530

    fixed the issue of refreshToken api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -134,7 +134,6 @@ const logoutUser = asyncHandler(async (req, res) => {
 })
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
-  console.log(req.cookies.refreshToken);
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
   if(!incomingRefreshToken){
     throw new ApiError(401,"Unauthorized Request!")
@@ -146,7 +145,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       process.env.REFRESH_TOKEN_SECRET
     )
 
-    const user = User.findById(decodedToken?._id)
+    const user = await User.findById(decodedToken?._id)
     if(!user){
       throw new ApiError(401,"Invalid Refresh Token!!")
     }
