f4047e68fb6dab560f70af6fced65d09f9e8543c
mangat-ram <se.mangat.ram@gmail.com>
2024-03-10 10:13:01 +0530

    completed task middleware testing pending.

---
+++ src/middlewares/mol.middleware.js
@@ -0,0 +1,3 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Molecule } from "../models/molecule.model.js";
--- src/middlewares/task.middleware.js
+++ src/middlewares/task.middleware.js
@@ -1,19 +1,28 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js";
 import { Task } from "../models/task.model.js";
 
 export const verifyTask = asyncHandler(async(req, res, next) => {
-  const user = req.user;
-  const userId = user._id;
-  if(!user){
-    throw new ApiError(404,"User in verifyTask not Found!!!")
-  }
-  const currentTask = await Task.findOne(
-    {
-      creator:userId,
-      
+  try {
+    const user = req.user;
+    const userId = user._id;
+    const labname = req.params.labname;
+    const title = req.params.title;
+    if(!(user && labname && taskTitle)){
+      throw new ApiError(404,"User,labname or title in verifyTask not Found!!!")
     }
-  )
+    const currentTask = await Task.findOne(
+      {
+        creator:userId,
+        labname:labname,
+        title:title
+      }
+    )
+  
+    req.task = currentTask;
+    next()
+  } catch (error) {
+    throw new ApiError(401,error?.message || "Something went wrong in catch of task middleware")
+  }
 
 })
